7b045a708e051fb80973c13a66dd9ac81a804606
Puzanov Alexandr <leifon2011@gmail.com>
2023-05-21 21:42:37 +0300

    Ficks

--- lab8-3.py
+++ lab8-3.py
@@ -2,17 +2,17 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 name = input("Введите имя получателя: ")
-filename = "postcard.jpg"
+filename = "23_февраля.jpg"
 with Image.open(filename) as img:
     img.load()
 # используется файл с жирными символами шрифта Arial
 font = ImageFont.truetype("arial_bold.ttf", 25)
 draw_text = ImageDraw.Draw(img)
 draw_text.text(
-    (img.width // 2 - 100, 15),
+    (img.width // 2 - 50, 415),
     name + ", поздравляю!",
     font=font,
-    fill=('#FAACAC')
-)
+    fill=('#FAACAC'))
+
 img.show()
 img.save(name + "_postcard.png")
